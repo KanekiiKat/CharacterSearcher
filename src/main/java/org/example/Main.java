364b8ef5fcaa8692111c2ed8bacde4f2bab7a9a4
KanekiiCat <alex123viri@gmail.com>
2025-09-16 21:31:56 +0200

    Avanzar Tarea 1.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
 package org.example;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
 2. Búsqueda de carácteres
 
@@ -10,8 +17,37 @@ Variante: dado un fichero encuentre el carácter más usado.
 
 public class Main {
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escribe aquí el nombre del directorio: ");
+        String inputFile = sc.nextLine();
+
+        System.out.println("Escribe aquí una letra (Si no escribes ninguna, se sacara el caracteres más usado): ");
+
+        String inputChar = sc.nextLine();
+
+        Path f = new File(inputFile).toPath();
+
+        if (f.toFile().exists()) {
+            if (inputChar.equals("")) {
+                characterMaxRepeatCount(f);
+            } else {
+
+            }
+        } else {
+            System.out.println("El archivo no existe.");
+        }
+
 
     }
 
-}
+    static void characterMaxRepeatCount(Path file) throws IOException {
+        String text = Files.readString(file);
+        char[] charArray = text.toCharArray();
+
+        }
+    }
+
